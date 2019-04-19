3aaddb96c26dce88bbf67abd64a8ff5795bdc476
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-04-19 14:30:06 +0530

    chore: add react webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/react',
+            [
+              '@babel/env',
+              {
+                targets: {
+                  browsers: ['last 2 versions']
+                }
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  }
+}
