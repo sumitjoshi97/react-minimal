5e53a9e54c91fb3302264ca2111b4a3a7f1b1da9
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-04-23 17:58:56 +0530

    chore: move index file to src folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
