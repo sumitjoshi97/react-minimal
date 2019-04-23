181e6d47e07b5d6592eebcead1401be1e207599e
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-04-23 18:00:57 +0530

    chore: add app component

---
+++ src/App/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './styles.scss'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h2>Minimal React Boiler</h2>
+      </div>
+    )
+  }
+}
