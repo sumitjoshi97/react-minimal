50852a9478683bc8d7bf4622d07e601da0878d66
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-04-25 18:57:23 +0530

    chore: add build optimations to webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,10 @@
 const path = require('path')
-const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWeppackPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const postcssSafeParser = require('postcss-safe-parser')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const merge = require('webpack-merge')
 const base = require('./webpack.config.base')
 
@@ -45,10 +48,59 @@ module.exports = merge(base, {
       }
     ]
   },
+  optimization: {
+    minimizer: [
+      new TerserWeppackPlugin({
+        terserOptions: {
+          parse: {
+            ecma: 8
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2
+          },
+          mangle: {
+            safari10: true
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true
+          }
+        },
+        parallel: true,
+        cache: true,
+        sourceMap: true
+      })
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
+      template: path.join(__dirname, 'public', 'index.html'),
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAtributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
     }),
-    new MiniCssExtractPlugin({ filename: 'static/css/[name].[hash:8].css' })
+    new MiniCssExtractPlugin({
+      filename: 'static/css/[name].[hash:8].css'
+    }),
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorOptions: {
+        parser: postcssSafeParser,
+        map: true
+      }
+    }),
+    new CleanWebpackPlugin()
   ]
 })
