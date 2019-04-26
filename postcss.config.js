22de3c709ae7b6f7debd7cae3124a9af88585030
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-04-26 17:49:30 +0530

    chore: update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
 const autoprefixer = require('autoprefixer')
 
 module.exports = {
-  plugins: [
-    autoprefixer({
-      browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
-    })
-  ]
+  plugins: [autoprefixer()],
 }
