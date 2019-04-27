3660e9623af3d72db321aa77395ab0679d99b8bd
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-04-27 16:12:35 +0530

    chore: remove unused variables

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,7 +1,4 @@
 const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const DotenvPlugin = require('dotenv-webpack')
 
 module.exports = {
