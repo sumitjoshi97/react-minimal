466c09d2544748bd31c26123bee9f50e2fc2ca38
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-01 11:28:10 +0530

    chore:  fix non-js imports for jest

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,11 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
   testPathIgnorePatterns: ['\\\\node_modules\\\\'],
   testURL: 'http://localhost',
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   verbose: false,
 }
