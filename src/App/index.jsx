021dd377e9a6fef470995024591da637203e1b5c
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-06-18 11:33:53 +0530

    refactor: convert class component to function

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './styles.scss'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <h2>minimal react boiler</h2>
-        <img src={require('../assets/logo.png')} alt="react boiler" />
-        <a
-          href="https://github.com/sumitjoshi97/react-boiler"
-          rel="noreferrer noopener"
-          target="_blank"
-        >
-          know more
-        </a>
-        <footer>developed and maintained by @Sj</footer>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className="app">
+    <h2>minimal react boiler</h2>
+    <img src={require('../assets/logo.png')} alt="react boiler" />
+    <a
+      href="https://github.com/sumitjoshi97/react-boiler"
+      rel="noreferrer noopener"
+      target="_blank"
+    >
+      know more
+    </a>
+    <footer>developed and maintained by @Sj</footer>
+  </div>
+)
+
+export default App
